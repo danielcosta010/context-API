56087a7dd0bed19cba964dac8bc2dec2b47ea79d
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-09-02 19:20:40 -0300

    Atualizando CarrinhoContext.jsx

--- src/context/CarrinhoContext.jsx
+++ src/context/CarrinhoContext.jsx
@@ -1,21 +1,41 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useMemo, useReducer, useState } from "react";
+import { carrinhoReducer } from "../reducers/carrinhoReducer";
 
 export const CarrinhoContext = createContext();
 CarrinhoContext.displayName = "Carrinho";
 
+const estadoInicial = [];
+
 export const CarrinhoProvider = ({ children }) => {
-  const [carrinho, setCarrinho] = useState([]);
+  const [carrinho, dispatch] = useReducer(carrinhoReducer, estadoInicial);
   const [quantidade, setQuantidade] = useState(0);
   const [valorTotal, setValorTotal] = useState(0);
+
+  const { totalTemp, quantidadeTemp } = useMemo(() => {
+    return carrinho.reduce((acumulador, produto) => (
+      {
+        totalTemp: acumulador.totalTemp + produto.quantidade * produto.preco,
+        quantidadeTemp: acumulador.quantidadeTemp + produto.quantidade,
+      }),
+      {
+        totalTemp: 0,
+        quantidadeTemp: 0,
+      }
+    )
+  }, [carrinho])
+
+  useEffect(() => {
+    setValorTotal(totalTemp);
+    setQuantidade(quantidadeTemp);
+  })
+
   return (
     <CarrinhoContext.Provider
       value={{
         carrinho,
-        setCarrinho,
+        dispatch,
         quantidade,
-        setQuantidade,
         valorTotal,
-        setValorTotal,
       }}
     >
       {children}
